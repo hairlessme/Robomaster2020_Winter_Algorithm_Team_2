e50f1674e7cc0435fa315801fb1e90eb079f4ba9
qiujiangkun <qiujiangkun@foxmail.com>
2020-02-03 18:18:33 +0800

    accurate path

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/opt/ros/kinetic'.split(';')
+            CMAKE_PREFIX_PATH = '/mnt/hgfs/Projects/Robomaster2020_Winter_Algorithm_Team_2/devel;/opt/ros/kinetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
